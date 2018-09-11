2a960bfa7e37d9cf31a8ec6d54e8ad5147039649
Sergey Taran <taransergey@gmail.com>
2018-09-11 18:36:20 +0300

    Improve test compatibility with python 3

--- tests/test_get_nearest_line.py
+++ tests/test_get_nearest_line.py
@@ -1,41 +1,41 @@
-from io import BytesIO
+from io import StringIO
 from botstat.botstat import get_nearest_line
 
 
 def test_first():
-    stream = BytesIO("123\n"
-                     "456\n"
-                     "789\n")
-    assert get_nearest_line(stream, 0) == "123\n"
+    stream = StringIO(u"123\n"
+                      u"456\n"
+                      u"789\n")
+    assert get_nearest_line(stream, 0) == u"123\n"
 
 
 def test_first_in_the_middle():
-    stream = BytesIO("123\n"
-                     "456\n"
-                     "789\n")
+    stream = StringIO(u"123\n"
+                      u"456\n"
+                      u"789\n")
     stream.seek(2)
-    assert get_nearest_line(stream, 0) == "123\n"
+    assert get_nearest_line(stream, 0) == u"123\n"
 
 
 def test_second_in_the_middle():
-    stream = BytesIO("123\n"
-                     "456\n"
-                     "789\n")
+    stream = StringIO(u"123\n"
+                      u"456\n"
+                      u"789\n")
     stream.seek(5)
-    assert get_nearest_line(stream, 0) == "456\n"
+    assert get_nearest_line(stream, 0) == u"456\n"
 
 
 def test_second_in_the_less_start():
-    stream = BytesIO("123\n"
-                     "456\n"
-                     "789\n")
+    stream = StringIO(u"123\n"
+                      u"456\n"
+                      u"789\n")
     stream.seek(5)
     assert get_nearest_line(stream, 6) is None
 
 
 def test_last_in_the_middle():
-    stream = BytesIO("123\n"
-                     "456\n"
-                     "789\n")
+    stream = StringIO(u"123\n"
+                      u"456\n"
+                      u"789\n")
     stream.seek(10)
-    assert get_nearest_line(stream, 0) == "789\n"
+    assert get_nearest_line(stream, 0) == u"789\n"
--- tests/test_seek_to_date.py
+++ tests/test_seek_to_date.py
@@ -1,6 +1,6 @@
 from datetime import date
 import re
-from io import BytesIO
+from io import StringIO
 from botstat.botstat import seek_to_date
 
 
@@ -9,122 +9,122 @@ RE_PARSER = re.compile(r'.*\[(?P<time_local>.*)\].*')
 
 
 def test_in_the_middle():
-    stream = BytesIO(
-        "1 [2018/09/01 12:09:09]\n"
-        "2 [2018/09/02 12:09:10]\n"
-        "3 [2018/09/03 12:09:11]\n"
-        "4 [2018/09/03 12:09:19]\n"
-        "5 [2018/09/03 12:09:29]\n"
-        "6 [2018/09/04 12:09:09]\n"
-        "7 [2018/09/04 12:19:09]\n"
+    stream = StringIO(
+        u"1 [2018/09/01 12:09:09]\n"
+        u"2 [2018/09/02 12:09:10]\n"
+        u"3 [2018/09/03 12:09:11]\n"
+        u"4 [2018/09/03 12:09:19]\n"
+        u"5 [2018/09/03 12:09:29]\n"
+        u"6 [2018/09/04 12:09:09]\n"
+        u"7 [2018/09/04 12:19:09]\n"
     )
     seek_to_date(stream, DATE, RE_PARSER)
-    assert stream.readline() == "3 [2018/09/03 12:09:11]\n"
+    assert stream.readline() == u"3 [2018/09/03 12:09:11]\n"
 
 
 def test_first_line():
-    stream = BytesIO(
-        "3 [2018/09/03 12:09:09]\n"
-        "4 [2018/09/03 12:09:19]\n"
-        "5 [2018/09/03 12:09:29]\n"
-        "6 [2018/09/04 12:09:09]\n"
-        "7 [2018/09/04 12:19:09]\n"
+    stream = StringIO(
+        u"3 [2018/09/03 12:09:09]\n"
+        u"4 [2018/09/03 12:09:19]\n"
+        u"5 [2018/09/03 12:09:29]\n"
+        u"6 [2018/09/04 12:09:09]\n"
+        u"7 [2018/09/04 12:19:09]\n"
     )
     seek_to_date(stream, DATE, RE_PARSER)
-    assert stream.readline() == "3 [2018/09/03 12:09:09]\n"
+    assert stream.readline() == u"3 [2018/09/03 12:09:09]\n"
 
 
 def test_last_line():
-    stream = BytesIO(
-        "1 [2018/09/01 12:09:09]\n"
-        "2 [2018/09/02 12:09:09]\n"
-        "3 [2018/09/03 12:09:09]\n"
+    stream = StringIO(
+        u"1 [2018/09/01 12:09:09]\n"
+        u"2 [2018/09/02 12:09:09]\n"
+        u"3 [2018/09/03 12:09:09]\n"
     )
     seek_to_date(stream, DATE, RE_PARSER)
-    assert stream.readline() == "3 [2018/09/03 12:09:09]\n"
+    assert stream.readline() == u"3 [2018/09/03 12:09:09]\n"
 
 
 def test_at_the_end():
-    stream = BytesIO(
-        "1 [2018/09/01 12:09:09]\n"
-        "2 [2018/09/02 12:09:09]\n"
-        "3 [2018/09/03 12:09:09]\n"
-        "4 [2018/09/03 12:09:19]\n"
-        "5 [2018/09/03 12:09:29]\n"
+    stream = StringIO(
+        u"1 [2018/09/01 12:09:09]\n"
+        u"2 [2018/09/02 12:09:09]\n"
+        u"3 [2018/09/03 12:09:09]\n"
+        u"4 [2018/09/03 12:09:19]\n"
+        u"5 [2018/09/03 12:09:29]\n"
     )
     seek_to_date(stream, DATE, RE_PARSER)
-    assert stream.readline() == "3 [2018/09/03 12:09:09]\n"
+    assert stream.readline() == u"3 [2018/09/03 12:09:09]\n"
 
 
 def test_in_the_middle_not_exact():
-    stream = BytesIO(
-        "1 [2018/09/01 12:09:09]\n"
-        "2 [2018/09/02 12:09:09]\n"
-        "6 [2018/09/04 12:09:09]\n"
-        "7 [2018/09/04 12:09:19]\n"
-        "8 [2018/09/05 12:09:09]\n"
+    stream = StringIO(
+        u"1 [2018/09/01 12:09:09]\n"
+        u"2 [2018/09/02 12:09:09]\n"
+        u"6 [2018/09/04 12:09:09]\n"
+        u"7 [2018/09/04 12:09:19]\n"
+        u"8 [2018/09/05 12:09:09]\n"
     )
     seek_to_date(stream, DATE, RE_PARSER)
-    assert stream.readline() == "6 [2018/09/04 12:09:09]\n"
+    assert stream.readline() == u"6 [2018/09/04 12:09:09]\n"
 
 
 def test_first_line_not_exact():
-    stream = BytesIO(
-        "6 [2018/09/04 12:09:09]\n"
-        "7 [2018/09/04 12:19:09]\n"
-        "8 [2018/09/05 12:09:09]\n"
+    stream = StringIO(
+        u"6 [2018/09/04 12:09:09]\n"
+        u"7 [2018/09/04 12:19:09]\n"
+        u"8 [2018/09/05 12:09:09]\n"
     )
     seek_to_date(stream, DATE, RE_PARSER)
-    assert stream.readline() == "6 [2018/09/04 12:09:09]\n"
+    assert stream.readline() == u"6 [2018/09/04 12:09:09]\n"
 
 
 def test_last_line_not_exact():
-    stream = BytesIO(
-        "1 [2018/09/01 12:09:09]\n"
-        "2 [2018/09/02 12:09:09]\n"
-        "6 [2018/09/04 12:09:09]\n"
+    stream = StringIO(
+        u"1 [2018/09/01 12:09:09]\n"
+        u"2 [2018/09/02 12:09:09]\n"
+        u"6 [2018/09/04 12:09:09]\n"
     )
     seek_to_date(stream, DATE, RE_PARSER)
-    assert stream.readline() == "6 [2018/09/04 12:09:09]\n"
+    assert stream.readline() == u"6 [2018/09/04 12:09:09]\n"
 
 
 def test_at_the_end_not_exact():
-    stream = BytesIO(
-        "1 [2018/09/01 12:09:09]\n"
-        "2 [2018/09/02 12:09:09]\n"
-        "6 [2018/09/04 12:09:09]\n"
-        "7 [2018/09/04 12:19:09]\n"
-        "8 [2018/09/05 12:09:09]\n"
+    stream = StringIO(
+        u"1 [2018/09/01 12:09:09]\n"
+        u"2 [2018/09/02 12:09:09]\n"
+        u"6 [2018/09/04 12:09:09]\n"
+        u"7 [2018/09/04 12:19:09]\n"
+        u"8 [2018/09/05 12:09:09]\n"
     )
     seek_to_date(stream, DATE, RE_PARSER)
-    assert stream.readline() == "6 [2018/09/04 12:09:09]\n"
+    assert stream.readline() == u"6 [2018/09/04 12:09:09]\n"
 
 
 def test_all_are_less():
-    stream = BytesIO(
-        "1 [2018/09/01 12:09:09]\n"
-        "2 [2018/09/02 12:09:09]\n"
-        "3 [2018/09/02 12:19:09]\n"
-        "4 [2018/09/02 12:29:19]\n"
+    stream = StringIO(
+        u"1 [2018/09/01 12:09:09]\n"
+        u"2 [2018/09/02 12:09:09]\n"
+        u"3 [2018/09/02 12:19:09]\n"
+        u"4 [2018/09/02 12:29:19]\n"
     )
     seek_to_date(stream, DATE, RE_PARSER)
-    assert stream.readline() == ""
+    assert stream.readline() == u""
     position = stream.tell()
     assert position == stream.seek(0, 2) # check is eof
 
 
 def test_all_are_larger():
-    stream = BytesIO(
-        "1 [2018/09/05 12:09:09]\n"
-        "2 [2018/09/06 12:09:09]\n"
-        "3 [2018/09/07 12:19:09]\n"
-        "4 [2018/09/08 12:29:19]\n"
+    stream = StringIO(
+        u"1 [2018/09/05 12:09:09]\n"
+        u"2 [2018/09/06 12:09:09]\n"
+        u"3 [2018/09/07 12:19:09]\n"
+        u"4 [2018/09/08 12:29:19]\n"
     )
     seek_to_date(stream, DATE, RE_PARSER)
-    assert stream.readline() == "1 [2018/09/05 12:09:09]\n"
+    assert stream.readline() == u"1 [2018/09/05 12:09:09]\n"
 
 
 def test_empty():
-    stream = BytesIO("")
+    stream = StringIO(u"")
     seek_to_date(stream, DATE, RE_PARSER)
-    assert stream.readline() == ""
+    assert stream.readline() == u""
